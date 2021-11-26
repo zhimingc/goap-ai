f9f2a0a0b4f55ccf5b88388a27d020c44ec896ac
Zhiming Chen <zhi.m.ch@gmail.com>
2021-11-26 13:25:47 +1100

    GOAP class structure

---
+++ Scripts/Action.gd
@@ -0,0 +1,14 @@
+class_name Action
+
+var preconditions = {}
+var effects = {}
+var cost = 0.0
+
+func check_preconditions(actor : Actor):
+	pass
+	
+func perform(actor : Actor):
+	pass
+	
+func is_in_range(actor : Actor):
+	return true
